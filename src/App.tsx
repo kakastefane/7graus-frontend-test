807f99c8d33edb69b105a532079c1b659c24023e
kakastefane <karistonstefane@hotmail.com>
2022-03-11 15:25:29 -0300

    created a simple html, started to consume api with axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import api from "./services/api";
 
 function App() {
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    async function getUsers() {
+      const response = await api.get("api/", {
+        params: {
+          gender: "male",
+          results: 10,
+          page: 1,
+        },
+      });
+      const data = response.data;
+    }
+    getUsers();
+  }, []);
+
   return (
-    <div className="App">
+    <>
       <h1>Hello</h1>
-    </div>
+      <section className="users">
+        <div className="user">
+          <img
+            className="user__image"
+            src="https://placeimg.com/100/100/people"
+            alt=""
+          />
+          <div className="user__info">
+            <h3 className="user__name">Nome</h3>
+            <span className="user__email">email</span>
+          </div>
+        </div>
+        <div className="user">
+          <img
+            className="user__image"
+            src="https://placeimg.com/100/100/people"
+            alt=""
+          />
+          <div className="user__info">
+            <h3 className="user__name">Nome</h3>
+            <span className="user__email">email</span>
+          </div>
+        </div>
+        <div className="user">
+          <img
+            className="user__image"
+            src="https://placeimg.com/100/100/people"
+            alt=""
+          />
+          <div className="user__info">
+            <h3 className="user__name">Nome</h3>
+            <span className="user__email">email</span>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
